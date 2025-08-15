895b45705727acb5717650317591f113a5f504e5
christiankencana <christiankencana28@gmail.com>
2025-08-15 08:40:55 +0700

    change userseeder

--- app/Database/Seeds/UsersSeeder.php
+++ app/Database/Seeds/UsersSeeder.php
@@ -3,17 +3,26 @@
 namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
+use App\Models\UserModel;
 
 class UsersSeeder extends Seeder
 {
     public function run()
     {
-        // $this->db->table('users')->truncate();
-        $this->db->table('users')->insert([
-            'email' => 'it5@amt.co.id',
-            'password' => password_hash('1234', PASSWORD_DEFAULT),
-            'name' => 'Stephen',
-            'created_at' => date('Y-m-d H:i:s')
+        // $this->db->table('users')->insert([
+        //     'email' => 'it5@amt.co.id',
+        //     'password' => password_hash('1234', PASSWORD_DEFAULT),
+        //     'name' => 'Stephen',
+        //     'created_at' => date('Y-m-d H:i:s')
+        // ]);
+
+        $userModel = new UserModel();
+        $userModel->insert([
+            'email'       => 'it5@amt.co.id',
+            'password'    => password_hash('1234', PASSWORD_DEFAULT),
+            'name'        => 'Stephen',
+            'last_login'  => date('Y-m-d H:i:s'),
+            'created_at'  => date('Y-m-d H:i:s')
         ]);
     }
 }
